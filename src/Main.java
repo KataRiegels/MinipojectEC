6cf8f45d268ff4faed8cfbd3d9e6e92c1a38be42
stine-fohrmann <Lotte547>
2020-11-16 11:02:18 +0100

    new classes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        /*
         int ave = 0;
         int iter = 1;
         for (int i = 0; i < iter; i++) {
@@ -16,12 +16,23 @@ public class Main {
         ave = ave/iter;
 
         System.out.println("average winning: " + ave);
+        */
 
+        // welcome/starting message
+        System.out.println("Welcome to Blackjack!");
+        System.out.println("Do you know the rules or would you like me to explain them?");
 
-
+        String answer = readString();
+        Case case1 = new Case(answer);
+        Reply reply1 = new Reply(case1.getTrigger());
 
     }
 
+    // method that reads input as a string
+    public static String readString() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
 
 }
 
