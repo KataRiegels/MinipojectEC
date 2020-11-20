5e549f5641ff35b1c20c53762a97bb57352af638
stine-fohrmann <Lotte547>
2020-11-20 19:00:27 +0100

    km

--- src/Output.java
+++ src/Output.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Output {
    String reply;
+   ArrayList<String> additionalDisplay;
    String[][] keywords; // keywords that trigger this particular output
+   Output[] allOutputs;
    Output[] possibleOutputs;
    //ArrayList<Output> possibleOutputs;
    Output defaultOutput; // = new Output("starting the game");
@@ -19,6 +21,7 @@ public class Output {
    // are checked first (before keywords for other outputs) in getNext()
    // setting the default keywords that always lead to starting the game
    public void setDefaultKeywords() {
+      this.keywords = new String[0][0];
       String trigger1[] = {"start", "game"}; // more default triggers can be defined here
       ArrayList<String[]> triggers = new ArrayList<>();
       triggers.add(trigger1);
@@ -34,12 +37,35 @@ public class Output {
       return this.keywords;
    }
 
+   public Output[] getPossibleOutputs() {
+      return possibleOutputs;
+   }
+
    // set possible outputs (method) - Output[] as input for this method
    // define the default Output, then the other possible Outputs
    // in getNext() check for default Output first, then the other Outputs
-   public void setPossibleOutputs(Output[] possibleOutputs) {
+   /*public void setPossibleOutputs(Output[] possibleOutputs) {
       defaultOutput = new Output("starting the game");
       this.possibleOutputs = possibleOutputs;
+   }*/
+
+   // setPossibleOutputs method with undefined number of Outputs as input
+   public void setPossibleOutputs(Output ... possibleOutputs) {
+      defaultOutput = new Output("starting the game");
+
+      /*if (possibleOutputs == null) {
+         this.possibleOutputs = new Output[]{defaultOutput};
+      } else {
+         this.possibleOutputs = new Output[possibleOutputs.length];
+         for (int i = 0; i < possibleOutputs.length; i++) {
+            this.possibleOutputs[i] = possibleOutputs[i];
+         }
+      }*/
+
+      this.possibleOutputs = new Output[possibleOutputs.length];
+      for (int i = 0; i < possibleOutputs.length; i++) {
+         this.possibleOutputs[i] = possibleOutputs[i];
+      }
    }
 
 /*
@@ -62,8 +88,13 @@ public class Output {
       if (containsTrigger(defaultKeywords, splitInput)) {
          return defaultOutput;
       }
+
       // if player doesn't want to start the game yet, get next output
       for (Output r : possibleOutputs) {
+         // check if there are any triggers
+         /*if (this.getKeywords().length == 0) {
+            return r;
+         }*/
          if (containsTrigger(r.getKeywords(), splitInput)) {
             //System.out.println("worked");
             return r;
@@ -92,7 +123,30 @@ public class Output {
       return false;
    }
 
+   // method that adds/performs any actions specific to the output
+   // define actions
+
+   public void setAdditionalDisplay(String ... strings){
+      additionalDisplay = new ArrayList<String>();
+      for (String s : strings) {
+         additionalDisplay.add(s);
+      }
+   }
+
+   // copy method that copies Output object with all its attributes etc.
+   public Output copy() {
+      Output newOutput = new Output(this.reply);
+      newOutput.setKeyword(this.getKeywords());
+      newOutput.setPossibleOutputs(this.possibleOutputs);
+      return newOutput;
+   }
+
    public void print() {
       System.out.println(this.reply);
+      if (additionalDisplay != null) {
+         for (String s : additionalDisplay) {
+            System.out.println(s);
+         }
+      }
    }
 }
